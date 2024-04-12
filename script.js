9c862042b28385534c72756b31939a3b5ae24555
chonnabotimpexcambodia <163649310+chonnabotimpexcambodia@users.noreply.github.com>
2024-04-12 18:55:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function home()
 {
-    document.getElementById('home').style.display= "block";
+    document.getElementById('services').style.display= "none";
     document.getElementById('contact').style.display= "none";
-    document.getElementById('about').style.display= "none";
 }
-function about()
+function services()
 {
-    document.getElementById('about').style.display= "block";
-    document.getElementById('home').style.display= "none";
+    document.getElementById('services').style.display= "block";
     document.getElementById('contact').style.display= "none";
+    document.getElementById('home').style.display= "none";
 }
 function contact()
 {
     document.getElementById('contact').style.display= "block";
-    document.getElementById('about').style.display= "none";
+    document.getElementById('services').style.display= "none";
     document.getElementById('home').style.display= "none";
 }
